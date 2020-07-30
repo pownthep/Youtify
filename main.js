502de578c58b753485fb7836425089fd84f9cfc2
Pownthep Laokhunthot <pownthepofficial@gmail.com>
2020-07-29 22:56:16 -0700

    update dependencies and update ui

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function createWindow() {
             nodeIntegration: true
         }
     })
+    win.setMenuBarVisibility(false)
 
     // and load the index.html of the app.
     win.loadFile('index.html')
